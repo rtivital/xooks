b9a856b06e25c638e3340b53843927b29d4831c8
Vitaly Rtishchev <rtivital@gmail.com>
2020-09-03 10:55:19 +0300

    Fix broken webpack builds

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,10 @@
 module.exports = {
+  presets: [['@babel/preset-env', { modules: false }]],
+  plugins: ['@babel/plugin-proposal-export-default-from'],
+
   env: {
     test: {
       presets: ['@babel/preset-env'],
-      plugins: ['@babel/plugin-proposal-export-default-from'],
-    },
-
-    production: {
-      presets: [['@babel/preset-env', { modules: false }]],
       plugins: ['@babel/plugin-proposal-export-default-from'],
     },
   },
