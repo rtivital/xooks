e07fe6392e4d84b462d9aaa5b72c35aeb0642f0e
Vitaly Rtishchev <rtivital@gmail.com>
2020-12-10 12:11:11 +0300

    [docs] Add basic links styles

--- docs/src/components/Navbar/Navbar.tsx
+++ docs/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import cx from 'clsx';
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.styles.less';
 
 export default function Navbar({ className }: { className?: string }) {
@@ -9,7 +10,18 @@ export default function Navbar({ className }: { className?: string }) {
         <h1 className={classes.logo}>Xooks</h1>
         <p className={classes.description}>General purpose React hooks collection</p>
       </header>
-      <div className={classes.body}>Body</div>
+      <div className={classes.body}>
+        <NavLink to="/" exact className={classes.link} activeClassName={classes.linkActive}>
+          Getting Started
+        </NavLink>
+        <NavLink
+          to="/use-document-title"
+          className={classes.link}
+          activeClassName={classes.linkActive}
+        >
+          use-document-title
+        </NavLink>
+      </div>
       <footer className={classes.footer}>Footer</footer>
     </nav>
   );
