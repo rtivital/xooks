c0c3d08d503cd648f9c2c3fdf9ef2f3c4e2064ff
Vitaly Rtishchev <rtivital@gmail.com>
2021-01-08 16:30:43 +0300

    Fix incorrect dist publishing to npm

--- docs/src/routes/getting-started/GettingStarted.route.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Markdown from '../../components/Markdown/Markdown';
-import docs from './GettingStarted.md';
-
-export default function GettingStartedRoute() {
-  return <Markdown>{docs}</Markdown>;
-}
