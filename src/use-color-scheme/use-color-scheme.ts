cc6f9da3d34a8f9fdbdc40ad9e3c6c8cb64c96dc
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-11 16:38:35 +0300

    use-color-scheme: Fix incorrect ssr logic

--- src/use-color-scheme/use-color-scheme.ts
+++ src/use-color-scheme/use-color-scheme.ts
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react';
-
-const media = window.matchMedia('(prefers-color-scheme: dark)');
+import { useState, useRef, useEffect } from 'react';
 
 export default function useColorScheme() {
-  const [scheme, setScheme] = useState<'dark' | 'light'>(media.matches ? 'dark' : 'light');
+  const media = useRef<MediaQueryList>();
+  const [scheme, setScheme] = useState<'dark' | 'light'>(media.current.matches ? 'dark' : 'light');
   const handleSchemeChange = (query: { matches: boolean }) =>
     setScheme(query.matches ? 'dark' : 'light');
 
   useEffect(() => {
-    media.addEventListener('change', handleSchemeChange);
-    return () => media.removeEventListener('change', handleSchemeChange);
+    media.current = window.matchMedia('(prefers-color-scheme: dark)');
+    media.current.addEventListener('change', handleSchemeChange);
+    return () => media.current.removeEventListener('change', handleSchemeChange);
   }, []);
 
   return scheme;
